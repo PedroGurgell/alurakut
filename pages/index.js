cf5965202101dabaabd8aa074117efac964ae209
PedroGurgell <71411486+PedroGurgell@users.noreply.github.com>
2021-07-19 13:18:14 -0300

    Trocando a URL de requisição da API para a da vercel

--- pages/index.js
+++ pages/index.js
@@ -159,7 +159,7 @@ export async function getServerSideProps(context) {
   const cookies = nookies.get(context);
   const token = cookies.USER_TOKEN;
   const { githubUser } = jwt.decode(token);
-  const { isAuthenticated } = await fetch('http://localhost:3000/api/auth', {
+  const { isAuthenticated } = await fetch('alurakut-pedrogurgell.vercel.app/api/auth', {
     headers: {
         Authorization: token
       }
